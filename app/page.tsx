dc11779d6a2d2afb32d726c9ba4832e220d3851e
Carlos Tovar <carlos.tovar@cocoasystems.net>
2025-02-20 14:10:04 -0400

    Added authenticator

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 
@@ -12,7 +14,8 @@ Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
-export default function App() {
+export function App() {
+    const { signOut } = useAuthenticator();
     const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
     function listTodos() {
@@ -52,6 +55,17 @@ export default function App() {
                 <br />
                 <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">Review next steps of this tutorial.</a>
             </div>
+            <button onClick={signOut}>Sign out</button>
         </main>
+    );
+}
+
+export default function Main() {
+    return (
+        <React.StrictMode>
+            <Authenticator>
+                <App />
+            </Authenticator>
+        </React.StrictMode>
     );
 }
