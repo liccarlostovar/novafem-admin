31123f58f8f3fd52617692b735a4b47955eba119
Carlos Tovar <carlos.tovar@cocoasystems.net>
2025-02-20 11:57:23 -0400

    Second commit

--- app/page.tsx
+++ app/page.tsx
@@ -13,40 +13,45 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 export default function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+    const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }
+    function listTodos() {
+        client.models.Todo.observeQuery().subscribe({
+            next: (data) => setTodos([...data.items]),
+        });
+    }
 
-  useEffect(() => {
-    listTodos();
-  }, []);
+    useEffect(() => {
+        listTodos();
+    }, []);
 
-  function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
-    });
-  }
+    const createTodo = () => {
+        client.models.Todo.create({
+            content: window.prompt("Todo content"),
+        });
+    };
+    const deleteTodo = (id: string) => {
+        client.models.Todo.delete({ id });
+    };
 
-  return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
-          Review next steps of this tutorial.
-        </a>
-      </div>
-    </main>
-  );
+    return (
+        <main>
+            <h1>My todos</h1>
+            <button onClick={createTodo}>+ new</button>
+            <ul>
+                {todos.map((todo) => (
+                    <li
+                        key={todo.id}
+                        onClick={() => deleteTodo(todo.id)}>
+                        {todo.content}
+                    </li>
+                ))}
+            </ul>
+            <div>
+                🥳 App successfully hosted. Try creating a new todo.
+                <br />
+                <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">Review next steps of this tutorial.</a>
+            </div>
+        </main>
+    );
 }
