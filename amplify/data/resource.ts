ffc7f3fb838cfa7e7858dd829b1370d28d7eaa5f
Carlos Tovar <carlos.tovar@cocoasystems.net>
2025-02-20 14:35:51 -0400

    Todos auth changes

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -7,23 +7,25 @@ specifies that any user authenticated via an API key can "create", "read",
 "update", and "delete" any "Todo" records.
 =========================================================================*/
 const schema = a.schema({
-  Todo: a
-    .model({
-      content: a.string(),
-    })
-    .authorization((allow) => [allow.publicApiKey()]),
+    Todo: a
+        .model({
+            content: a.string(),
+        })
+        .authorization((allow) => [allow.owner()]),
+    // .authorization((allow) => [allow.publicApiKey()]),
 });
 
 export type Schema = ClientSchema<typeof schema>;
 
 export const data = defineData({
-  schema,
-  authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
-    apiKeyAuthorizationMode: {
-      expiresInDays: 30,
+    schema,
+    authorizationModes: {
+        // defaultAuthorizationMode: "apiKey",
+        defaultAuthorizationMode: "userPool",
+        apiKeyAuthorizationMode: {
+            expiresInDays: 30,
+        },
     },
-  },
 });
 
 /*== STEP 2 ===============================================================
--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ Amplify.configure(outputs);
 const client = generateClient<Schema>();
 
 const App = () => {
-    const { signOut } = useAuthenticator();
+    const { user, signOut } = useAuthenticator();
     const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
     function listTodos() {
@@ -39,7 +39,7 @@ const App = () => {
 
     return (
         <main>
-            <h1>My todos</h1>
+            <h1>{user?.signInDetails?.loginId}'s todos</h1>
             <button onClick={createTodo}>+ new</button>
             <ul>
                 {todos.map((todo) => (
